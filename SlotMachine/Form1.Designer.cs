387b7049d0dda5c3cbacfae414e57eeeb8c6e9c6
KassandraSaint <kassandrastripper@gmail.com>
2022-06-19 21:35:31 -0400

    Add project files.

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inserted = new System.Windows.Forms.TextBox();
+            this.spinBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox1.Location = new System.Drawing.Point(34, 32);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(125, 125);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox2.Location = new System.Drawing.Point(209, 32);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(125, 125);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 1;
+            this.picBox2.TabStop = false;
+            // 
+            // picBox3
+            // 
+            this.picBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox3.Location = new System.Drawing.Point(383, 32);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(125, 125);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox3.TabIndex = 2;
+            this.picBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount Inserted: $";
+            // 
+            // inserted
+            // 
+            this.inserted.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inserted.Location = new System.Drawing.Point(329, 195);
+            this.inserted.Name = "inserted";
+            this.inserted.Size = new System.Drawing.Size(138, 39);
+            this.inserted.TabIndex = 4;
+            this.inserted.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // spinBtn
+            // 
+            this.spinBtn.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spinBtn.Location = new System.Drawing.Point(95, 264);
+            this.spinBtn.Name = "spinBtn";
+            this.spinBtn.Size = new System.Drawing.Size(160, 60);
+            this.spinBtn.TabIndex = 5;
+            this.spinBtn.Text = "Spin";
+            this.spinBtn.UseVisualStyleBackColor = true;
+            this.spinBtn.Click += new System.EventHandler(this.spinBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Font = new System.Drawing.Font("Mongolian Baiti", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(281, 264);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(160, 60);
+            this.exitBtn.TabIndex = 6;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.spinBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(538, 358);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.spinBtn);
+            this.Controls.Add(this.inserted);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Name = "Form1";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inserted;
+        private System.Windows.Forms.Button spinBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        // Crearting a SIZE constant
+        const int SIZE = 3;
+        // Creating a dictionary to hold pictures for slot machine
+        private Dictionary<int, string> pics = new Dictionary<int, string>()
+        {
+            {0, "Apple.bmp"}, 
+            {1, "Banana.bmp"},
+            {2, "Cherries.bmp"},
+            {3, "Grapes.bmp"},
+            {4, "Lemon.bmp"},
+            {5, "Lime.bmp"},
+            {6, "Orange.bmp"},
+            {7, "Pear.bmp"},
+            {8, "Strawberry.bmp"},
+            {9, "Watermelon.bmp"}
+        };
+
+        // Initilizing an array to hold randomly selected pictures
+        int[] randomPicNums = new int[SIZE];
+
+        // Initializing total variables to account money in and money out
+        decimal moneyIn = 0m;
+        decimal moneyOut = 0m;
+
+        // Creating Random Object
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Creating a method to generate random picture numbers and fill pictureBox controls with random pictures
+        private void picGenerator()
+        {
+            // Creating an array of pictureBoxes
+            PictureBox[] pictureBoxes = new PictureBox[SIZE] { picBox1, picBox2, picBox3 }; 
+
+            // Choosing 3 random pictures and adding them to randomPics array
+            for (int i = 0; i < SIZE; i++)
+            {
+                int pic = rand.Next(pics.Count);
+                randomPicNums[i] = pic;
+
+                // Getting elements from the pics dictionary  at selected random positions 
+                var picture = pics[randomPicNums[i]];
+
+                // Filling picture Box slots with random pictures 
+                pictureBoxes[i].Image = new Bitmap("Resources/" + picture);
+            }        
+        }
+
+        // Creating a method to count how many pictures matched
+        private int matchedCount()
+        {
+            // Initializing a variable to track number of matched pictures and setting it to 1 as it
+            // will always have at least one occurace 
+            int numMatched = 1;
+
+            // Creating a dictionary to track number of elements
+            Dictionary<int, int> counter = new Dictionary<int, int>();
+
+            // Itterating over array of random pictures, adding them to a dictionary counter
+            for (int i = 0; i < SIZE; i++)
+            {
+                // If counter dictionary doesn't countain an element with the key from randomPicNums array
+                if (!counter.ContainsKey(randomPicNums[i]))
+                {
+                    // Adding an element with this number as a key and setting a value to 1
+                    counter[randomPicNums[i]] = 1;
+                }
+                // Otherwise, if it is already present incrementing value by 1
+                else
+                {
+                    counter[randomPicNums[i]]++;
+                    // If key exists in the dictionary and we incresing it's value counter,
+                    // we increase numMatched counnter by one as well
+                    numMatched++;
+                }
+            }
+
+            // Returning number of matched pictures
+            return numMatched;
+        }
+
+        // Creating a method to count how much was won
+        private void moneyPerSpin()
+        {
+            // Initializing money variable to store user inserted money
+            decimal money = 0;
+            // Calling matchedCount method to get number of matched pictures and store it in matched variable
+            int matched = matchedCount();
+            // Getting amount of money entered by a user and storing it in money variable
+            if (decimal.TryParse(inserted.Text, out money))
+            {
+                // Updating moneyIn variable by incrementing it by current money value
+                moneyIn += money;
+                // If 2 or 3 pictures matched multiplying money inserted by 2 or 3 respectively
+                if (matched == 3)
+                {
+                    money *= 3;
+                }
+                else if (matched == 2)
+                {
+                    money *= 2;
+                }
+                // Or setting money to 0
+                else
+                {
+                    money = 0;
+                }
+                // Updating moneyOut variable by incrementing it by current money value
+                moneyOut += money;
+
+                // Creating a message variable formatted with format specifier for curency
+                string message = String.Format("You got {0:C} after this spin", money);
+                // Showing user how much money he or she got after a spin
+                MessageBox.Show(message);
+            }
+            else
+            {
+                MessageBox.Show("Wrong input! Try entering amount of inserted money again");
+            }
+        }
+        private void spinBtn_Click(object sender, EventArgs e)
+        {
+            // Calling picGenerator method to generate images for slot machine
+            picGenerator();
+
+            // Calling moneyPerSpin method to calculate how much was won or lost over a slot machine spin
+            moneyPerSpin();
+
+        }
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // Showing a message to the user on how much money was won and lost during a game
+            string message = String.Format("You inserted {0:C} into a slot machine and won {1:C}", moneyIn, moneyOut);
+            MessageBox.Show(message);
+            // Closing the form 
+            this.Close();
+        }
+    }
+}
